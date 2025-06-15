4777383832ab1310f6a7884bdcdf80a3fa76b343
Aryangitrepo <aryanpatkar99@gmail.com>
2025-06-15 21:25:01 +0530

    fixed some bugs

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -2,7 +2,6 @@ package jwt
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
 
@@ -10,15 +9,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func loadEnv() {
+func loadEnv() error {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file" + err.Error())
+		return err
 	}
+	return nil
 }
 
 func CreateToken(username string) (string, error) {
-	loadEnv()
-	secretKey := os.Getenv("SECRET_KEY")
+	var secretKey string
+	if err := loadEnv(); err.Error() == "Error loading .env fileopen .env: no such file or directory" {
+		secretKey = "hello"
+	}
+	secretKey = os.Getenv("SECRET_KEY")
 	if secretKey == "" {
 		secretKey = "hello"
 	}
